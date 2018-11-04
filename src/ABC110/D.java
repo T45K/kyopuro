5f5e8a6f2354596229a8d77572d107d7b942c5ad
tsk-nkgw <tasktas9@gmail.com>
2018-11-04 13:24:40 +0900

    ABC110 D

---
+++ src/ABC110/D.java
@@ -0,0 +1,17 @@
+package ABC110;
+
+import java.util.Scanner;
+
+public class D {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Scanner scanner = new Scanner(System.in);
+		int length = scanner.nextInt();
+		int multi = scanner.nextInt();
+		
+		
+
+	}
+
+}
