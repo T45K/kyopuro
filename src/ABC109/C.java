a84e8183960db24a06374d23dfea8eb8ae283b5f
tsk-nkgw <tasktas9@gmail.com>
2019-02-13 18:19:44 +0900

    delete CE

--- src/ABC109/C.java
+++ src/ABC109/C.java
@@ -5,31 +5,31 @@ import java.util.Scanner;
 
 public class C {
 
-	public static void main(String[]  args)  {
+	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int counter = scanner.nextInt();
 		int start = scanner.nextInt();
 		int[] point = new int[counter];
-		
-		for(int i = 0;i<counter;i++) {
-			point[i]= Math.abs(scanner.nextInt() - start); 
+
+		for (int i = 0; i < counter; i++) {
+			point[i] = Math.abs(scanner.nextInt() - start);
 		}
-		
+
 		Arrays.sort(point);
-		
-		for(int i = counter -1;i>0;i--) {
-			getGCD(point,i-1,i);
+
+		for (int i = counter - 1; i > 0; i--) {
+			getGCD(point, i - 1, i);
 		}
-		
+
 		System.out.println(point[0]);
 		scanner.close();
 	}
-	
-	public static void getGCD(int[] a,int x,int y) {
+
+	public static void getGCD(int[] a, int x, int y) {
 		int tmp;
-		while((tmp = a[y] % a[x]) != 0) {
-			a[y]= a[x];
-			a[x]= tmp; 
+		while ((tmp = a[y] % a[x]) != 0) {
+			a[y] = a[x];
+			a[x] = tmp;
 		}
 	}
 }
