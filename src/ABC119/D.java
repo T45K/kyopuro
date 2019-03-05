21e6d05a8a0aa4fcbc30186144bf6fc774c10415
tsk-nkgw <tasktas9@gmail.com>
2019-03-05 14:16:28 +0900

    update

--- src/ABC119/D.java
+++ src/ABC119/D.java
@@ -1,6 +1,8 @@
 package ABC119;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class D {
@@ -10,41 +12,77 @@ public class D {
 		final int shrine = scanner.nextInt();
 		final int temple = scanner.nextInt();
 		final int startPoint = scanner.nextInt();
-		final long[] shrines = new long[shrine];
-		final long[] temples = new long[temple];
+		final List<Long> shrines = new ArrayList<>();
+		final List<Long> temples = new ArrayList<>();
 
 		for (int i = 0; i < shrine; i++) {
-			shrines[i] = scanner.nextLong();
+			shrines.add(scanner.nextLong());
 		}
 
 		for (int i = 0; i < temple; i++) {
-			temples[i] = scanner.nextLong();
+			temples.add(scanner.nextLong());
 		}
 
-		Arrays.sort(shrines);
-		Arrays.sort(temples);
+		Collections.sort(shrines);
+		Collections.sort(temples);
 
-		boolean shr = true;
-		long l = Long.MAX_VALUE, r = 0;
-		int sIndex = 0, tIndex = 0;
-		long result = Long.MAX_VALUE;
-		int rlIndex = Integer.MAX_VALUE, rrIndex = 0;
+		boolean isLeftShrine;
 
-		for (int count = 0; count < shrine + temple; count++) {
-			if (shrines[sIndex] < temples[tIndex]) {
-				if (shr) {
-					l = shrines[sIndex++];
+		long shortestDiff = Long.MAX_VALUE;
+		long left;
+		long resultLeft = 0, resultRight = 0;
+
+		if (shrines.get(0) < temples.get(0)) {
+			isLeftShrine = true;
+			left = shrines.get(0);
+			shrines.remove(0);
+		} else {
+			isLeftShrine = false;
+			left = temples.get(0);
+			temples.remove(0);
+		}
+
+		while (shrines.size() > 0 && temples.size() > 0) {
+			if (shrines.get(0) < temples.get(0)) {
+				if (isLeftShrine) {
+					left = shrines.get(0);
+					shrines.remove(0);
 				} else {
-
+					if (shrines.get(0) - left < shortestDiff) {
+						resultLeft = left;
+						resultRight = shrines.get(0);
+						shortestDiff = resultRight - resultLeft;
+					}
+					left = shrines.get(0);
+					isLeftShrine = true;
+					shrines.remove(0);
 				}
 			} else {
-				l = temples[tIndex];
-				tIndex++;
+				if (!isLeftShrine) {
+					left = temples.get(0);
+					temples.remove(0);
+				} else {
+					if (temples.get(0) - left < shortestDiff) {
+						resultLeft = left;
+						resultRight = temples.get(0);
+						shortestDiff = resultRight - resultLeft;
+					}
+					left = temples.get(0);
+					isLeftShrine = false;
+					temples.remove(0);
+				}
 
 			}
 		}
+		
+		final List<String> results = new ArrayList<>();
+		for (int i = 0; i < startPoint; i++) {
+			long index = scanner.nextLong();
+			results.add(Long.toString(shortestDiff + Math.min(Math.abs(index - resultLeft), Math.abs(index - resultRight))));
+		}
 
+		System.out.println(String.join("\n", results));
 		scanner.close();
 	}
-
+//check answer, later...
 }
