b461b110ee10fabb717714a8e5ead56f53ce4721
unknown <tasktas9@gmail.com>
2019-05-26 01:30:50 +0900

    add package

--- ABC127/C.java
+++ ABC127/C.java
@@ -1,3 +1,5 @@
+package ABC127;
+
 import java.util.Scanner;
 
 public class C {
--- ABC127/D.java
+++ ABC127/D.java
@@ -1,3 +1,5 @@
+package ABC127;
+
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Scanner;
