29b72edb19939e399ac3094b334aca60d918a982
tsk-nkgw <tasktas9@gmail.com>
2019-05-25 22:36:06 +0900

    update

---
+++ ABC127/A.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class A {
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final int a = scanner.nextInt();
+        final int b = scanner.nextInt();
+
+        if(a >= 13){
+            System.out.println(b);
+        }else if (a >= 6){
+            System.out.println(b/2);
+        }else{
+            System.out.println(0);
+        }
+    }
+}
---
+++ ABC127/E.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class E {
+
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final int counter = scanner.nextInt();
+        scanner.nextInt();
+
+        long a = scanner.nextInt();
+        long cons = -1 * a + scanner.nextInt();
+        long mina = a;
+        long maxa = a;
+        int count = 1;
+
+        for (int i = 0; i < counter - 1; i++) {
+            if (scanner.nextInt() == 1) {
+                final long tmp = scanner.nextInt();
+                cons -= tmp;
+                mina = Math.min(tmp, mina);
+                maxa = Math.max(tmp, maxa);
+
+                cons += scanner.nextInt();
+                count++;
+            } else {
+                System.out.println(mina + " " + (maxa * count + cons));
+            }
+        }
+
+    }
+}
