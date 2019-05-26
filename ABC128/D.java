47bed69f0ae22d28813178149e44c3c75dc128db
tsk-nkgw <tasktas9@gmail.com>
2019-05-26 19:36:53 +0900

    hinagata

---
+++ ABC128/D.java
@@ -0,0 +1,9 @@
+package ABC128;
+
+import java.util.Scanner;
+
+public class D {
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+    }
+}
---
+++ ABC128/E.java
@@ -0,0 +1,9 @@
+package ABC128;
+
+import java.util.Scanner;
+
+public class E {
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+    }
+}
