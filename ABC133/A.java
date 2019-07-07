ee95bcadb9d1632e96e278a02316b17d2f6f1218
tsk-nkgw <tasktas9@gmail.com>
2019-07-07 23:05:54 +0900

    update

---
+++ ABC133/A.java
@@ -0,0 +1,14 @@
+package ABC133;
+
+import java.util.Scanner;
+
+public class A {
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final int i = scanner.nextInt();
+        final int j = scanner.nextInt();
+        final int k = scanner.nextInt();
+
+        System.out.println(Math.min(i * j, k));
+    }
+}
