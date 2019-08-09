4b09ba189d36f4df64bc5d528babb8860e6c1207
T45K <tasktas9@gmail.com>
2019-08-09 10:36:41 +0900

    update

--- ABC128/D.java
+++ ABC128/D.java
@@ -1,6 +1,9 @@
 package ABC128;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class D {
@@ -9,21 +12,44 @@ public class D {
         final int queueLength = scanner.nextInt();
         final int operation = scanner.nextInt();
 
-        final int[] queue = new int[queueLength];
-        final int[] sort = new int[queueLength];
-        int all = 0;
-        for(int i = 0;i<queueLength;i++){
-            final int tem = scanner.nextInt();
-            queue[i] = tem;
-            sort[i] = tem;
-            all += tem;
+        final List<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < queueLength; i++) {
+            queue.add(scanner.nextInt());
         }
 
-        Arrays.sort(sort);
+        final int counter = Math.min(queueLength, operation);
+        int res = 0;
 
-        int result = Integer.MIN_VALUE;
+        for (int i = 0; i <= counter; i++) {
+            for (int j = 0; j <= i; j++) {
+                final LinkedList<Integer> clonedQueue = new LinkedList<>(queue);
+                final List<Integer> selection = new ArrayList<>();
+                for (int k = 0; k < j; k++) {
+                    selection.add(clonedQueue.pollFirst());
+                }
+                for (int k = j; k < i; k++) {
+                    selection.add(clonedQueue.pollLast());
+                }
+                Collections.sort(selection);
+                for (int l = 0; l < operation - i; l++) {
+                    if (selection.isEmpty()) {
+                        break;
+                    }
 
-        for(int i = 0;i<queueLength;i++){
+                    if (selection.get(0) < 0) {
+                        selection.remove(0);
+                    } else {
+                        break;
+                    }
+                }
+
+                final int sum = selection.stream().mapToInt(Integer::intValue).sum();
+                if (sum > res) {
+                    res = sum;
+                }
+            }
         }
+        System.out.println(res);
     }
 }
