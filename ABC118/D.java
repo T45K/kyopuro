7e1c9ccb3e66682da97ae38e7f13e5bd1c42379e
T45K <tasktas9@gmail.com>
2019-08-21 22:57:58 +0900

    update

--- ABC118/D.java
+++ ABC118/D.java
@@ -1,103 +1,128 @@
 package ABC118;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class D {
 
-	public static void main(String[] args) {
-		final Scanner scanner = new Scanner(System.in);
-		final int stick = scanner.nextInt();
-		final int availableNumber = scanner.nextInt();
+    private static final int[] requirement = {-1, 2, 5, 5, 4, 5, 6, 3, 7, 6};
 
-		final List<Integer> numbers = new ArrayList<>();
-		for (int i = 0; i < availableNumber; i++) {
-			numbers.add(scanner.nextInt());
-		}
+    public static void main(final String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final int n = scanner.nextInt();
+        final int m = scanner.nextInt();
 
-		minimize5Match(numbers);
-		minimize6Match(numbers);
-		minimizeSum7Match(numbers);
-		minimizeSum6Match(numbers);
-		minimizeSum5Match(numbers);
-		minimizeSum4Match(numbers);
-		
-		scanner.close();
-	}
+        final Set<Integer> numbers = new HashSet<>();
+        for (int i = 0; i < m; i++) {
+            numbers.add(scanner.nextInt());
+        }
 
-	// 1 2 3 4 5 6 7 8 9
-	// 2 5 5 4 5 6 3 7 6
-	public static void minimizeSum4Match(List<Integer> list) {
-		if(list.contains(4)) {
-			if(list.contains(1)) {
-				list.remove(list.indexOf(4));
-			}
-		}
-	}
-	
-	public static void m(List<Integer> list, int m) {
-		if (list.contains(m)) {
-			if (list.contains(1) && list.contains(7)) {
-				list.remove(list.indexOf(m));
-			}
-		}
-	}
+        removeRedundantNumber(numbers);
+        final List<Tuple> tuples = numbers.stream()
+                .map(e -> new Tuple(e, requirement[e]))
+                .sorted((o1, o2) -> o2.getOriginalNumber() - o1.getOriginalNumber())
+                .collect(Collectors.toList());
 
-	public static void minimizeSum5Match(List<Integer> list) {
-		m(list, 5);
-		m(list, 2);
-		m(list, 3);
-	}
+        final String[][] dp = new String[tuples.size()][n + 1];
+        dp[0][0] = "";
 
-	public static void minimizeSum6Match(List<Integer> list) {
-		if (list.contains(6)) {
-			if (list.contains(1) || list.contains(7)) {
-				list.remove(list.indexOf(6));
-			}
-		}
-	}
+        for (int i = 0; i < tuples.size(); i++) {
+            for (int j = 0; j < n + 1; j++) {
+                if (dp[i][j] != null) {
+                    if (j < n - tuples.get(i).getNumOfMatches() + 1) {
+                        final String tmp = dp[i][j] + tuples.get(i).getOriginalNumberString();
+                        if (dp[i][j + tuples.get(i).getNumOfMatches()] == null || originalStringCompare(tmp, dp[i][j + tuples.get(i).getNumOfMatches()])) {
+                            dp[i][j + tuples.get(i).getNumOfMatches()] = tmp;
+                        }
+                    }
 
-	public static void minimizeSum7Match(List<Integer> list) {
-		if (list.contains(8)) {
-			if ((list.contains(1) || list.contains(4)) && list.contains(7)) {
-				list.remove(list.indexOf(8));
-				return;
-			}
+                    if (i != tuples.size() - 1 && j < n - tuples.get(i + 1).getNumOfMatches() + 1) {
+                        final String tmp = dp[i][j] + tuples.get(i + 1).getOriginalNumberString();
+                        if (dp[i + 1][j + tuples.get(i + 1).getNumOfMatches()] == null || originalStringCompare(tmp, dp[i + 1][j + tuples.get(i + 1).getNumOfMatches()]))
+                            dp[i + 1][j + tuples.get(i + 1).getNumOfMatches()] = tmp;
+                    }
 
-			if (list.contains(1) && (list.contains(2) || list.contains(3) || list.contains(5))) {
-				list.remove(list.indexOf(8));
-				return;
-			}
-		}
-	}
+                    if (i != tuples.size() - 1) {
+                        dp[i + 1][j] = dp[i][j];
+                    }
+                }
+            }
+        }
 
-	public static void minimize5Match(List<Integer> list) {
-		if (list.contains(5)) {
-			int i;
-			if ((i = list.indexOf(3)) != -1) {
-				list.remove(i);
-			}
+        System.out.println(dp[tuples.size() - 1][n]);
+    }
 
-			if ((i = list.indexOf(2)) != -1) {
-				list.remove(i);
-			}
-			return;
-		}
+    private static boolean originalStringCompare(final String a, final String b) {
+        if (a.length() > b.length()) {
+            return true;
+        }
 
-		if (list.contains(3)) {
-			if (list.contains(2)) {
-				list.remove(list.indexOf(2));
-			}
-		}
-	}
+        if (b.length() > a.length()) {
+            return false;
+        }
 
-	public static void minimize6Match(List<Integer> list) {
-		if (list.contains(9)) {
-			if (list.contains(6)) {
-				list.remove(list.indexOf(6));
-			}
-		}
-	}
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) > b.charAt(i))
+                return true;
 
+            if (a.charAt(i) < b.charAt(i)) {
+                return false;
+            }
+        }
+
+        return false;
+    }
+
+    private static void removeRedundantNumber(final Set<Integer> numbers) {
+        if (numbers.contains(5)) {
+            numbers.remove(3);
+            numbers.remove(2);
+        }
+
+        if (numbers.contains(3)) {
+            numbers.remove(2);
+        }
+
+        if (numbers.contains(1)) {
+            numbers.remove(4);
+            numbers.remove(6);
+            numbers.remove(9);
+        }
+
+        if (numbers.contains(7)) {
+            numbers.remove(9);
+            numbers.remove(6);
+        }
+    }
+
+    static class Tuple {
+        private final int originalNumber;
+        private final int numOfMatches;
+
+        Tuple(final int originalNumber, final int numOfMatches) {
+            this.originalNumber = originalNumber;
+            this.numOfMatches = numOfMatches;
+        }
+
+        int getOriginalNumber() {
+            return originalNumber;
+        }
+
+        String getOriginalNumberString() {
+            return Integer.toString(originalNumber);
+        }
+
+        int getNumOfMatches() {
+            return numOfMatches;
+        }
+    }
+
+    // 1 2 3 4 5 6 7 8 9
+    // 2 5 5 4 5 6 3 7 6
 }
