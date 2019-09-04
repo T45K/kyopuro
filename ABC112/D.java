60aec5e0017b360f234af21ec4e96c676ec6d96c
T45K <tasktas9@gmail.com>
2019-09-05 03:25:40 +0900

    update

--- ABC112/D.java
+++ ABC112/D.java
@@ -4,30 +4,18 @@ import java.util.Scanner;
 
 public class D {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		Scanner scanner = new Scanner(System.in);
-		int a = scanner.nextInt();
-		int b = scanner.nextInt();
-		
-		if(a == 1) {
-			System.out.println(b);
-			return;
-		}
-	
-		for(int i = b/2;i>0;i--) {
-			if(b%i == 0) {
-				if(b/i >= a) {
-					System.out.println(i);
-					return;
-				}
-			}
-		}
-	}
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final int n = scanner.nextInt();
+        final int m = scanner.nextInt();
+
+        final int divide = m / n;
+
+        for (int i = divide; i >= 0; i--) {
+            if (m % i == 0 && m/i >= n) {
+                System.out.println(i);
+                return;
+            }
+        }
+    }
 }
-
-
-/*
-TLE
-約数をO(√M)で求める
-*/
