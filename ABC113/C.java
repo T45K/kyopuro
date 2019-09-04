7d0de66af04f2435f0d6b65f62f06a013f6e7e34
tsk-nkgw <tasktas9@gmail.com>
2019-09-04 11:25:15 +0900

    update

--- ABC113/C.java
+++ ABC113/C.java
@@ -1,38 +1,91 @@
 package ABC113;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class C {
+    public static void main(final String[] args) {
+        final FastScanner scanner = new FastScanner(System.in);
+        final int n = scanner.nextInt();
+        final int m = scanner.nextInt();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner(System.in);
-		int p = scanner.nextInt() + 1;
-		int counter = scanner.nextInt();
-		int[] aArray = new int[counter];
-		int[] bArray = new int[counter];
-		
-		ArrayList<Integer>[] arrayLists = new ArrayList[p];
-		for(int i = 0;i<counter;i++) {
-			int y = scanner.nextInt();
-			int t = scanner.nextInt();
-			aArray[i]= y;
-			bArray[i]= t; 
-			if(arrayLists[y] == null)
-				arrayLists[y] = new ArrayList<>(); 
-			arrayLists[y].add(t);
-		}
-		
-		for(int i = 0;i<p;i++) {
-			if(arrayLists[i]!= null && arrayLists[i].size() > 1 ) {
-				Collections.sort(arrayLists[i]);
-			}
-		}
-		
-		for(int i = 0;i<counter;i++) {
-			System.out.println(String.format("%012d", aArray[i] * 1000000 + arrayLists[aArray[i]].indexOf(bArray[i] ) + 1));
-		}
-	}
+        final Map<Integer, Integer> indexMap = new HashMap<>();
+        final Map<Integer, List<Integer>> prefectureCityMap = new HashMap<>();
+
+        for (int i = 0; i < m; i++) {
+            final int prefecture = scanner.nextInt();
+            final int city = scanner.nextInt();
+            indexMap.put(city, i);
+            prefectureCityMap.computeIfAbsent(prefecture, list -> new ArrayList<>())
+                    .add(city);
+        }
+
+        final String[] answer = new String[m];
+
+        for (final Map.Entry<Integer, List<Integer>> entry : prefectureCityMap.entrySet()) {
+            Collections.sort(entry.getValue());
+            for (int i = 0; i < entry.getValue().size(); i++) {
+                final String id = String.format("%06d", entry.getKey()) + String.format("%06d", i + 1);
+                answer[indexMap.get(entry.getValue().get(i))] = id;
+            }
+        }
+
+        for (final String s : answer) {
+            System.out.println(s);
+        }
+    }
+
+    static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+            tokenizer = null;
+        }
+
+        String next() {
+            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        String nextLine() {
+            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    return reader.readLine();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            return tokenizer.nextToken("\n");
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+    }
 }
