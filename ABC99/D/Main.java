a14d62db84950027dc444a81340de02c45d538f4
tsk-nkgw <tasktas9@gmail.com>
2019-09-20 00:54:28 +0900

    update

--- ABC99/D/Main.java
+++ ABC99/D/Main.java
@@ -8,14 +8,14 @@ public class Main {
         final int n = scanner.nextInt();
         final int c = scanner.nextInt();
 
-        final int[][] colorTable = new int[c][c];
+        final long[][] colorTable = new long[c][c];
         for (int i = 0; i < c; i++) {
             for (int j = 0; j < c; j++) {
-                colorTable[i][j] = scanner.nextInt();
+                colorTable[i][j] = scanner.nextLong();
             }
         }
 
-        final int[][] modTable = new int[3][c];
+        final long[][] modTable = new long[3][c];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -24,31 +24,23 @@ public class Main {
             }
         }
 
-        int answer = Integer.MAX_VALUE;
+        long answer = Long.MAX_VALUE;
         for (int i = 0; i < c; i++) {
-            int tmp = 0;
-            for (int i2 = 0; i2 < c; i2++) {
-                tmp += modTable[0][i2] * colorTable[i2][i];
-            }
-
             for (int j = 0; j < c; j++) {
-                if (i % 3 == j % 3) {
+                if (i == j) {
                     continue;
                 }
-
-                for (int j2 = 0; j2 < c; j2++) {
-                    tmp += modTable[1][j2] * colorTable[j2][j];
-                }
-
                 for (int k = 0; k < c; k++) {
-                    if (i % 3 == k % 3 || j % 3 == k % 3) {
+                    if (i == k || j == k) {
                         continue;
                     }
 
-                    for (int k2 = 0; k2 < c; k2++) {
-                        tmp += modTable[2][k2] * colorTable[k2][k];
+                    int tmp = 0;
+                    for (int l = 0; l < c; l++) {
+                        tmp += modTable[0][l] * colorTable[l][i];
+                        tmp += modTable[1][l] * colorTable[l][j];
+                        tmp += modTable[2][l] * colorTable[l][k];
                     }
-
                     answer = Math.min(answer, tmp);
                 }
             }
