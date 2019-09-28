79df3551057cfd1203080628f17e3c4e74844afc
T45K <tasktas9@gmail.com>
2019-09-28 22:54:05 +0900

    update

---
+++ ABC142/B/Main.java
@@ -0,0 +1,20 @@
+package ABC142.B;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(final String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final int n = scanner.nextInt();
+        final int k = scanner.nextInt();
+
+        int counter = 0;
+        for (int i = 0; i < n; i++) {
+            if (scanner.nextInt() >= k) {
+                counter++;
+            }
+        }
+
+        System.out.println(counter);
+    }
+}
