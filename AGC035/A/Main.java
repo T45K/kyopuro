82707927d7491f431a134f512112dcd1f551aa8e
tsk-nkgw <tasktas9@gmail.com>
2019-10-14 01:10:50 +0900

    update

---
+++ AGC035/A/Main.java
@@ -0,0 +1,16 @@
+package AGC035.A;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(final String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final int n = scanner.nextInt();
+        int tmp = 0;
+        for (int i = 0; i < n; i++) {
+            tmp ^= scanner.nextInt();
+        }
+
+        System.out.println(tmp == 0 ? "Yes" : "No");
+    }
+}
