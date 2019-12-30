49c3ac16fc512d4d82fc756dfa4491ca370807c4
T45K <tasktas9@gmail.com>
2019-12-31 01:07:45 +0900

    update

--- ABC149/E/Main.java
+++ ABC149/E/Main.java
@@ -1,44 +1,79 @@
 package ABC149.E;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    // TODO
     public static void main(final String[] args) {
         final Scanner scanner = new Scanner(System.in);
         final int n = scanner.nextInt();
-        final int m = scanner.nextInt();
+        final long m = scanner.nextLong();
 
-        final List<Long> list = new ArrayList<>();
-        long all = 0;
+        final long[] array = new long[n];
         for (int i = 0; i < n; i++) {
-            final long tmp = scanner.nextLong();
-            all += tmp * 2 * n;
-            list.add(tmp);
+            array[i] = scanner.nextInt();
         }
 
-        list.sort(Comparator.naturalOrder());
-
-        long minus = 0;
-
-        final int diff = n * n - m;
-        final int sqrt = (int) Math.sqrt(diff);
-        for (int i = 0; i < sqrt; i++) {
-            minus += 2 * sqrt * list.get(i);
+        Arrays.sort(array);
+        final long[] cumulativeSum = new long[n + 1];
+        for (int i = 0; i < n; i++) {
+            cumulativeSum[i + 1] += cumulativeSum[i] + array[i];
         }
 
-        final Long base = list.get(sqrt);
-        for (int i = 0; i < (diff - sqrt * sqrt) / 2; i++) {
-            minus += 2 * (base + list.get(i));
+        /*
+        ある値xに対して，その値を越える和の組み合わせが何通り存在するかを計算し，Mと比較して二分探索
+        各a_iに対して和の組み合わせはn通りある（a_i + a_0, a_i + a_1, ..., a_i + a_(n-1)）
+        このとき配列は昇順にソートされているので，a_i + a_j >= Xとなるjは，
+        a_j >= X - a_iを満たす最小のjの値をkとすると(n-k)個存在する（このkは二分探索で求められる）．
+        これを各a_iに対して計算していき，#jの総和(=何通りあるか)をとる．
+        この値がmより大きければxを大きくして，小さければxを小さくして再計算
+        */
+
+        long min = array[0] * 2;
+        long max = array[n - 1] * 2;
+        while (max - min > 1) {
+            long mid = (min + max) / 2;
+            long count = 0;
+            for (int i = 0; i < n; i++) {
+                final int index = upperBound(array, mid - array[i]) + 1;
+                count += n - index;
+            }
+            if (count >= m) {
+                min = mid;
+            } else {
+                max = mid;
+            }
         }
 
-        if ((diff - sqrt * sqrt) % 2 == 1) {
-            minus += base + list.get(diff - sqrt * sqrt);
+        long answer = 0;
+        long count = 0;
+        for (int i = 0; i < n; i++) {
+            final int index = upperBound(array, min - array[i]) + 1;
+            if (index == n) {
+                continue;
+            }
+
+            count += n - index;
+            // (a + b_1) + (a + b_2) + ... +  (a + b_n) = (b_1 + b_2 + ... + b_n) + n * a = (S_n - S_0) + n * a
+            answer += cumulativeSum[n] - cumulativeSum[index];
+            answer += array[i] * (n - index);
         }
 
-        System.out.println(all - minus);
+        answer += (m - count) * min;
+        System.out.println(answer);
+    }
+
+    private static int upperBound(final long[] array, final long value) {
+        int min = -1;
+        int max = array.length;
+        while (max - min > 1) {
+            int mid = (min + max) / 2;
+            if (array[mid] < value) {
+                min = mid;
+            } else {
+                max = mid;
+            }
+        }
+        return min;
     }
 }
