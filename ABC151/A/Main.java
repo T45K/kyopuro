745485c56ba85e693588830f7d7af57ffe998601
tsk-nkgw <tasktas9@gmail.com>
2020-01-12 23:33:16 +0900

    update

---
+++ ABC151/A/Main.java
@@ -0,0 +1,11 @@
+package ABC151.A;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(final String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final char c = scanner.next().charAt(0);
+        System.out.println((char) (c + 1));
+    }
+}
---
+++ ABC151/B/Main.java
@@ -0,0 +1,23 @@
+package ABC151.B;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(final String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final int n = scanner.nextInt();
+        final int k = scanner.nextInt();
+        final int m = scanner.nextInt();
+
+        int sum = 0;
+        for (int i = 0; i < n - 1; i++) {
+            sum += scanner.nextInt();
+        }
+
+        if (n * m > sum + k) {
+            System.out.println(-1);
+        } else {
+            System.out.println(Math.max(0, n * m - sum));
+        }
+    }
+}
