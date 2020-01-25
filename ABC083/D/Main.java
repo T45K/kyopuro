d5865c08ca4b0796aff107b04f1d8690f49fa01a
T45K <tasktas9@gmail.com>
2020-01-25 15:12:32 +0900

    update

---
+++ ABC083/D/Main.java
@@ -0,0 +1,20 @@
+package ABC083.D;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(final String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final String s = scanner.next();
+        int k = s.length();
+        char prev = s.charAt(0);
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) != prev) {
+                k = Math.min(k, Math.max(i, s.length() - i));
+                prev = s.charAt(i);
+            }
+        }
+
+        System.out.println(k);
+    }
+}
