27b113febaeeb0e1fc1fe32d7ed6ff4553a4dcae
tsk-nkgw <tasktas9@gmail.com>
2020-02-15 23:37:19 +0900

    solve

--- ABC073/D/Main.java
+++ ABC073/D/Main.java
@@ -1,14 +1,9 @@
 package ABC073.D;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class Main {
-    // TODO solve
     public static void main(final String[] args) {
         final Scanner scanner = new Scanner(System.in);
         final int n = scanner.nextInt();
@@ -20,70 +15,57 @@ public class Main {
             toVisit[i] = scanner.nextInt() - 1;
         }
 
-        final int[][] cities = new int[n][n];
+        final int[][] graph = new int[n][n];
+        for (int i = 0; i < graph.length; i++) {
+            Arrays.fill(graph[i], Integer.MAX_VALUE / 2);
+            graph[i][i] = 0;
+        }
+
         for (int i = 0; i < m; i++) {
             final int a = scanner.nextInt() - 1;
             final int b = scanner.nextInt() - 1;
             final int c = scanner.nextInt();
-            cities[a][b] = c;
-            cities[b][a] = c;
+
+            graph[a][b] = c;
+            graph[b][a] = c;
         }
 
-        int answer = Integer.MAX_VALUE;
-        for (int startIndex = 0; startIndex < r; startIndex++) {
-            for (int destinationIndex = startIndex + 1; destinationIndex < r; destinationIndex++) {
-                final int start = toVisit[startIndex];
-                final int destination = toVisit[destinationIndex];
-                final boolean[] isVisited = new boolean[n];
-                isVisited[start] = true;
-                final List<Integer> route = new ArrayList<>();
-                route.add(start);
-                final Set<Integer> sets = Arrays.stream(toVisit, 0, toVisit.length)
-                        .boxed()
-                        .filter(e -> e != start && e != destination)
-                        .collect(Collectors.toSet());
+        warshallFloyd(graph);
 
-                answer = Math.min(answer, recursive(start, destination, route, isVisited, cities, sets));
-            }
+        for (int i = 0; i < toVisit.length; i++) {
+            final boolean[] isVisited = new boolean[r];
+            isVisited[i] = true;
+            permutation(toVisit, isVisited, toVisit[i], graph, 1, 0);
         }
-
         System.out.println(answer);
     }
 
-    private static int recursive(final int current, final int destination, final List<Integer> route, final boolean[] isVisited, final int[][] cities, final Set<Integer> sets) {
-        if (current == destination) {
-            if (!sets.isEmpty()) {
-                return Integer.MAX_VALUE;
-            }
-            int sum = 0;
-            for (int i = 0; i < route.size() - 1; i++) {
-                final Integer prev = route.get(i);
-                final Integer next = route.get(i + 1);
-                if (cities[prev][next] == 0) {
-                    return Integer.MAX_VALUE;
-                }
-                sum += cities[prev][next];
-            }
-            return sum;
+    static int answer = Integer.MAX_VALUE;
+
+    private static void permutation(final int[] toVisit, final boolean[] isVisited, final int current, final int[][] graph, final int index, final int sum) {
+        if (index == isVisited.length) {
+            answer = Math.min(answer, sum);
+            return;
         }
 
-        int sum = Integer.MAX_VALUE;
         for (int i = 0; i < isVisited.length; i++) {
             if (isVisited[i]) {
                 continue;
             }
 
             isVisited[i] = true;
-            route.add(i);
-            final boolean isIncluded = sets.remove(i);
-            sum = Math.min(sum, recursive(i, destination, route, isVisited, cities, sets));
+            permutation(toVisit, isVisited, toVisit[i], graph, index + 1, sum + graph[current][toVisit[i]]);
             isVisited[i] = false;
-            route.remove(route.size() - 1);
-            if (isIncluded) {
-                sets.add(i);
+        }
+    }
+
+    private static void warshallFloyd(final int[][] graph) {
+        for (int k = 0; k < graph.length; k++) {
+            for (int i = 0; i < graph.length; i++) {
+                for (int j = 0; j < graph.length; j++) {
+                    graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+                }
             }
         }
-
-        return sum;
     }
 }
