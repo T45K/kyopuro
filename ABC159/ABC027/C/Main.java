c087fe252b0185db129c582d7598cbf0fb65d531
tsk-nkgw <tasktas9@gmail.com>
2020-03-27 17:52:28 +0900

    update

---
+++ ABC159/ABC027/C/Main.java
@@ -0,0 +1,49 @@
+package ABC159.ABC027.C;
+
+import java.util.Scanner;
+
+public class Main {
+    private static final String TAKAHASHI = "Takahashi";
+    private static final String AOKI = "Aoki";
+
+    public static void main(final String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        long n = scanner.nextLong();
+        final int digits = Long.toBinaryString(n).length() - 1;
+
+        long begin = 1L << digits;
+        long end = begin << 1L;
+        long mid = (begin + end) / 2L;
+        if (digits % 2 == 0) {
+            while (true) {
+                if (n < mid) {
+                    System.out.println(TAKAHASHI);
+                    return;
+                }
+                begin = mid;
+                mid = (begin + end) / 2L;
+                if (n >= mid) {
+                    System.out.println(AOKI);
+                    return;
+                }
+                end = mid;
+                mid = (begin + end) / 2L;
+            }
+        } else {
+            while (true) {
+                if (n >= mid) {
+                    System.out.println(TAKAHASHI);
+                    return;
+                }
+                end = mid;
+                mid = (begin + end) / 2L;
+                if (n < mid) {
+                    System.out.println(AOKI);
+                    return;
+                }
+                begin = mid;
+                mid = (begin + end) / 2L;
+            }
+        }
+    }
+}
