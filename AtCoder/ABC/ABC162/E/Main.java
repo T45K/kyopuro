115916d3880054dbac26a158d5890484352b46ff
T45K <tasktas9@gmail.com>
2020-04-13 11:07:29 +0900

    update

--- AtCoder/ABC/ABC162/E/Main.java
+++ AtCoder/ABC/ABC162/E/Main.java
@@ -1,12 +1,7 @@
 package AtCoder.ABC.ABC162.E;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
-// TODO solve
 public class Main {
     private static final long MOD = 1_000_000_007;
 
@@ -16,23 +11,16 @@ public class Main {
         final int k = scanner.nextInt();
 
         long sum = 0;
-        final List<Integer> primes = sieveOfEratosthenes(k);
         final long[] array = new long[k + 1];
         for (int i = k; i >= 1; i--) {
             final int nums = k / i;
-            final long multi = iterativePow(nums, n);
+            long multi = iterativePow(nums, n);
 
-            long tmp = 0;
-            for (final int prime : primes) {
-                if (i * prime > k) {
-                    break;
-                }
-                tmp += array[i * prime];
-                tmp %= MOD;
+            for (int j = 2; i * j <= k; j++) {
+                multi = (multi + MOD - array[i * j]) % MOD;
             }
 
-            final long diff = (multi + MOD - tmp) % MOD;
-            sum += diff * i % MOD;
+            sum += (multi * i) % MOD;
             sum %= MOD;
             array[i] = multi;
         }
@@ -49,33 +37,11 @@ public class Main {
                 tmp *= a;
             }
             a *= a;
+            a %= MOD;
             b >>= 1;
             tmp %= MOD;
         }
 
         return tmp;
-    }
-
-
-    private static List<Integer> sieveOfEratosthenes(final int number) {
-        List<Integer> numbers = IntStream.rangeClosed(2, number)
-                .boxed()
-                .collect(Collectors.toList());
-
-        final int sqrt = (int) Math.sqrt(number);
-        final List<Integer> primeNumbers = new ArrayList<>();
-        int condition;
-
-        do {
-            final int prime = numbers.get(0);
-            primeNumbers.add(prime);
-            numbers = numbers.stream()
-                    .filter(i -> i % prime != 0)
-                    .collect(Collectors.toList());
-            condition = prime;
-        } while (condition < sqrt);
-
-        primeNumbers.addAll(numbers);
-        return primeNumbers;
     }
 }
