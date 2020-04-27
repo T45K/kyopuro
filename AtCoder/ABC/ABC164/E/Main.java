afa60345b7aae3f1dbd0b94bb763509ea019fb3e
T45K <tasktas9@gmail.com>
2020-04-27 23:22:17 +0900

    update

--- AtCoder/ABC/ABC164/E/Main.java
+++ AtCoder/ABC/ABC164/E/Main.java
@@ -8,14 +8,20 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.OptionalLong;
 import java.util.PriorityQueue;
+import java.util.Set;
 import java.util.StringTokenizer;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
-// TODO solve
+/*
+ダイクストラ 通常の頂点数に加えて持っている金額という次元も加えた二次元配列のでの移動を考える
+解説AC こういうダイクストラもあるんやって感じ
+ */
 public class Main {
     public static void main(final String[] args) {
         final FastScanner scanner = new FastScanner(System.in);
@@ -23,15 +29,15 @@ public class Main {
         final int m = scanner.nextInt();
         final int s = scanner.nextInt();
 
-        final Map<Integer, List<Destination>> graph = new HashMap<>();
+        final Map<Integer, List<Edge>> graph = new HashMap<>();
         for (int i = 0; i < m; i++) {
             final int u = scanner.nextInt();
             final int v = scanner.nextInt();
             final int a = scanner.nextInt();
             final int b = scanner.nextInt();
 
-            graph.computeIfAbsent(u, value -> new ArrayList<>()).add(new Destination(u, v, a, b));
-            graph.computeIfAbsent(v, value -> new ArrayList<>()).add(new Destination(v, u, b, b));
+            graph.computeIfAbsent(u, value -> new ArrayList<>()).add(new Edge(v, a, b));
+            graph.computeIfAbsent(v, value -> new ArrayList<>()).add(new Edge(u, a, b));
         }
 
         final City[] cities = new City[n + 1];
@@ -41,46 +47,89 @@ public class Main {
             cities[i] = new City(c, d);
         }
 
-        final long[] answers = new long[n + 1];
-        Arrays.fill(answers, -1);
-        answers[1] = 0;
-        final PriorityQueue<Pair> queue = new PriorityQueue<>(Comparator.comparingLong(pair -> pair.destination.time));
-        queue.addAll(graph.get(1).stream().map(dest -> new Pair(s - dest.cost, dest)).collect(Collectors.toList()));
-        final City tmp = cities[1];
-        for (int i = 1; s + tmp.rate * i <= 2500; i++) {
-            queue.add(new Pair(s + i * tmp.rate, new Destination(1, 1, 0, i * tmp.time)));
+        final long[][] answers = new long[n + 1][2501];
+        for (final long[] array : answers) {
+            Arrays.fill(array, Long.MAX_VALUE);
         }
-        while (!queue.isEmpty()) {
-            final Pair poll = queue.poll();
-            if (poll.money < 0) {
-                continue;
+        final PriorityQueue<State> queue = new PriorityQueue<>(Comparator.comparingLong(state -> state.time));
+
+        int index;
+        for (index = 0; s + cities[1].rate * index < 2500; index++) {
+            final int currentMoney = (int) (s + cities[1].rate * index);
+            final long currentTime = cities[1].time * index;
+            answers[1][currentMoney] = currentTime;
+            for (final Edge next : graph.get(1)) {
+                if (currentMoney >= next.cost) {
+                    queue.add(new State(next.to, currentMoney - next.cost, currentTime + next.time));
+                }
             }
         }
+        answers[1][2500] = cities[1].time * index;
+        for (final Edge next : graph.get(1)) {
+            queue.add(new State(next.to, 2500 - next.cost, cities[1].time * index + next.time));
+        }
+
+        final Set<Integer> set = new HashSet<>();
+        set.add(1);
+        while (!queue.isEmpty()) {
+            final State state = queue.poll();
+            if (state.money < 0 || state.time >= answers[state.position][state.money]) {
+                continue;
+            }
+
+            final List<Edge> nextList = graph.get(state.position);
+            boolean flag = true;
+            for (index = 0; state.money + cities[state.position].rate * index < 2500; index++) {
+                final int currentMoney = (int) (state.money + cities[state.position].rate * index);
+                final long currentTime = state.time + cities[state.position].time * index;
+                if (currentTime >= answers[state.position][currentMoney]) {
+                    flag = false;
+                    break;
+                }
+
+                answers[state.position][currentMoney] = currentTime;
+                for (final Edge next : nextList) {
+                    queue.add(new State(next.to, currentMoney - next.cost, currentTime + next.time));
+                }
+            }
+            if (flag && state.time + cities[state.position].time * index < answers[state.position][2500]) {
+                answers[state.position][2500] = state.time + cities[state.position].time * index;
+                for (final Edge next : nextList) {
+                    queue.add(new State(next.to, 2500 - next.cost, answers[state.position][2500] + next.time));
+                }
+            }
+
+            set.add(state.position);
+            if (set.size() == n) {
+                break;
+            }
+        }
+
+        IntStream.rangeClosed(2, n)
+                .mapToObj(i -> Arrays.stream(answers[i]))
+                .map(LongStream::min)
+                .map(OptionalLong::orElseThrow)
+                .forEach(System.out::println);
     }
 
-    private static boolean isFilled(final long[] answers) {
-        return IntStream.range(1, answers.length)
-                .allMatch(i -> answers[i] >= 0);
-    }
+    private static class State {
+        final int position;
+        final int money;
+        final long time;
 
-    private static class Pair {
-        final long money;
-        final Destination destination;
-
-        Pair(final long money, final Destination destination) {
+        State(final int position, final int money, final long time) {
+            this.position = position;
             this.money = money;
-            this.destination = destination;
+            this.time = time;
         }
     }
 
-    private static class Destination {
-        final int from;
+    private static class Edge {
         final int to;
-        final long cost;
+        final int cost;
         final long time;
 
-        Destination(final int from, final int to, final long cost, final long time) {
-            this.from = from;
+        Edge(final int to, final int cost, final long time) {
             this.to = to;
             this.cost = cost;
             this.time = time;
