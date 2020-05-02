ffb12b259b4c472eed0b88af220c396a0fc9a41a
T45K <tasktas9@gmail.com>
2020-05-02 23:57:02 +0900

    rename

---
+++ AtCoder/ABC/ABC165/B/Main.java
@@ -0,0 +1,46 @@
+package AtCoder.ABC.ABC165.B;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(final String[] args) {
+        final FastScanner scanner = new FastScanner(System.in);
+        final long x = scanner.nextLong();
+        long tmp = 100;
+        for (int i = 1; i < 5000; i++) {
+            tmp = (long) ((double) tmp * 1.01d);
+            if (tmp >= x) {
+                System.out.println(i);
+                return;
+            }
+        }
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
---
+++ AtCoder/ABC/ABC165/D/Main.java
@@ -0,0 +1,47 @@
+package AtCoder.ABC.ABC165.D;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(final String[] args) {
+        final FastScanner scanner = new FastScanner(System.in);
+        final long a = scanner.nextLong();
+        final long b = scanner.nextLong();
+        final long n = scanner.nextLong();
+
+        if (b > n) {
+            System.out.println(a * n / b);
+        } else {
+            final long div = n / b * b - 1;
+            System.out.println(a * div / b - a * (div / b));
+        }
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
