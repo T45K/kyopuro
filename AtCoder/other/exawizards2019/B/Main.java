164fa0a07f713d251cda51b54147d6591d2dd556
T45K <tasktas9@gmail.com>
2020-08-26 14:08:29 +0900

    wip

---
+++ AtCoder/other/exawizards2019/B/Main.java
@@ -0,0 +1,26 @@
+package AtCoder.other.exawizards2019.B;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+        final char[] hats = scanner.nextLine().toCharArray();
+        int r = 0;
+
+        for (final char hat : hats) {
+            if (hat == 'R') {
+                r++;
+            }
+        }
+
+        if (r * 2 > hats.length) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
+        scanner.close();
+    }
+}
---
+++ AtCoder/other/exawizards2019/C/Main.java
@@ -0,0 +1,12 @@
+package AtCoder.other.exawizards2019.C;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final int n = scanner.nextInt();
+        final int q = scanner.nextInt();
+        final String s = scanner.next();
+    }
+}
