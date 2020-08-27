f8c909a46754ae058336166728c331e2d82ee0ef
T45K <tasktas9@gmail.com>
2020-08-27 22:05:57 +0900

    wip

---
+++ AtCoder/ARC/ARC058/A/Main.java
@@ -0,0 +1,56 @@
+package AtCoder.ARC.ARC058.A;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(final String[] args) {
+        final FastScanner scanner = new FastScanner(System.in);
+        final int n = scanner.nextInt();
+        final int k = scanner.nextInt();
+        final Set<Integer> d = IntStream.range(0, k)
+            .mapToObj(i -> scanner.nextInt())
+            .collect(Collectors.toSet());
+        for (int i = n; ; i++) {
+            final String s = Integer.toString(i);
+            final boolean any = IntStream.range(0, s.length())
+                .map(j -> s.charAt(j) - '0')
+                .anyMatch(d::contains);
+            if (!any) {
+                System.out.println(i);
+                return;
+            }
+        }
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
+    
