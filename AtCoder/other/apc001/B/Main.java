961a8143d53fe558641525608fe5330c22162c99
T45K <tasktas9@gmail.com>
2020-08-28 16:10:49 +0900

    update

---
+++ AtCoder/other/apc001/B/Main.java
@@ -0,0 +1,69 @@
+package AtCoder.other.apc001.B;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(final String[] args) {
+        final FastScanner scanner = new FastScanner(System.in);
+        final int n = scanner.nextInt();
+        final List<Integer> aList = IntStream.range(0, n)
+            .mapToObj(i -> scanner.nextInt())
+            .collect(Collectors.toList());
+        final List<Integer> bList = IntStream.range(0, n)
+            .mapToObj(i -> scanner.nextInt())
+            .collect(Collectors.toList());
+
+        long twoIncrease = 0;
+        long oneIncrease = 0;
+        for (int i = 0; i < n; i++) {
+            final int a = aList.get(i);
+            final int b = bList.get(i);
+            if (a >= b) {
+                oneIncrease += a - b;
+            } else if ((b - a) % 2 == 0) {
+                twoIncrease += (b - a) / 2;
+            } else {
+                twoIncrease += (b - a + 1) / 2;
+                oneIncrease++;
+            }
+        }
+
+        if (twoIncrease >= oneIncrease) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
+    
