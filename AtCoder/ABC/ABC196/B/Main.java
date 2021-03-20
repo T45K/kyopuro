90d42b41576977add69c8e2e9cb6f78dad546114
T45K <tasktas9@gmail.com>
2021-03-20 22:59:36 +0900

    update

---
+++ AtCoder/ABC/ABC196/B/Main.java
@@ -0,0 +1,42 @@
+package AtCoder.ABC.ABC196.B;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(final String[] args) {
+        final FastScanner scanner = new FastScanner(System.in);
+        final char[] x = scanner.next().toCharArray();
+        final String answer = IntStream.range(0, x.length)
+            .mapToObj(i -> x[i])
+            .takeWhile(Character::isDigit)
+            .map(Object::toString)
+            .collect(Collectors.joining());
+        System.out.println(answer);
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+    }
+}
