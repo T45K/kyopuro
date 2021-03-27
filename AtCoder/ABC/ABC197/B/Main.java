ba6f5621ac11947ccb33caee76beb14bd6b5cea7
T45K <tasktas9@gmail.com>
2021-03-27 22:47:18 +0900

    update

---
+++ AtCoder/ABC/ABC197/B/Main.java
@@ -0,0 +1,63 @@
+package AtCoder.ABC.ABC197.B;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(final String[] args) {
+        final FastScanner scanner = new FastScanner(System.in);
+        final int h = scanner.nextInt();
+        final int w = scanner.nextInt();
+        final int x = scanner.nextInt() - 1;
+        final int y = scanner.nextInt() - 1;
+        final boolean[][] table = new boolean[h][w];
+        for (int i = 0; i < h; i++) {
+            final String s = scanner.next();
+            for (int j = 0; j < w; j++) {
+                table[i][j] = s.charAt(j) == '.';
+            }
+        }
+
+        int count = 1;
+        for (int i = x - 1; i >= 0 && table[i][y]; i--) {
+            count++;
+        }
+        for (int i = x + 1; i < h && table[i][y]; i++) {
+            count++;
+        }
+        for (int j = y - 1; j >= 0 && table[x][j]; j--) {
+            count++;
+        }
+        for (int j = y + 1; j < w && table[x][j]; j++) {
+            count++;
+        }
+        System.out.println(count);
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
---
+++ AtCoder/ABC/ABC197/D/Main.java
@@ -0,0 +1,61 @@
+package AtCoder.ABC.ABC197.D;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+座標の移動
+x' = x*cos - y*sin
+y' = x*sin + y*cos
+ */
+public class Main {
+    public static void main(final String[] args) {
+        final FastScanner scanner = new FastScanner(System.in);
+        final int n = scanner.nextInt();
+        final double x0 = scanner.nextDouble();
+        final double y0 = scanner.nextDouble();
+        final double xn2 = scanner.nextDouble();
+        final double yn2 = scanner.nextDouble();
+
+        final double xc = (x0 + xn2) / 2;
+        final double yc = (y0 + yn2) / 2;
+
+        final double xRelative = x0 - xc;
+        final double yRelative = y0 - yc;
+        final double radian = 2 * Math.PI / n;
+        final double xAnswer = xRelative * Math.cos(radian) - yRelative * Math.sin(radian) + xc;
+        final double yAnswer = xRelative * Math.sin(radian) + yRelative * Math.cos(radian) + yc;
+        System.out.println(xAnswer + " " + yAnswer);
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+    }
+}
