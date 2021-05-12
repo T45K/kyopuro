71a39d659344578a48ed40c06f5ffce3d7e4b083
T45K <tasktas9@gmail.com>
2021-05-12 17:02:07 +0900

    update

---
+++ AtCoder/other/typecal90/T/Main.java
@@ -0,0 +1,53 @@
+package AtCoder.other.typecal90.T;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(final String[] args) {
+        final FastScanner scanner = new FastScanner(System.in);
+        final long a = scanner.nextLong();
+        final long b = scanner.nextInt();
+        final long c = scanner.nextInt();
+        final long big = Stream.generate(() -> c)
+            .limit(b)
+            .reduce(1L, (x, y) -> x * y);
+        if (a < big) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
