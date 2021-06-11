3707f4d71d8373940d9e018c4d6dce47c9908194
T45K <tasktas9@gmail.com>
2021-06-11 20:50:45 +0900

    update

---
+++ AtCoder/other/typecal90/ax050/Main.java
@@ -0,0 +1,72 @@
+package AtCoder.other.typecal90.ax050;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static final long MOD = 1_000_000_007;
+
+    public static void main(final String[] args) {
+        final FastScanner scanner = new FastScanner(System.in);
+        final int n = scanner.nextInt();
+        final int l = scanner.nextInt();
+        final long[] array = new long[n + 1];
+        array[0] = 1;
+        for (int i = 0; i < n; i++) {
+            array[i + 1] += array[i];
+            array[i + 1] %= MOD;
+            if (i + l <= n) {
+                array[i + l] += array[i];
+                array[i + l] %= MOD;
+            }
+        }
+        System.out.println(array[n]);
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    return reader.readLine();
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            return tokenizer.nextToken("\n");
+        }
+    }
+}
