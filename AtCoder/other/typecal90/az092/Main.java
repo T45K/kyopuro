7e37e08848e4d18dd0990e9592f14d6cbcc6149d
T45K <tasktas9@gmail.com>
2021-06-14 19:00:26 +0900

    update

--- AtCoder/other/typecal90/az092/Main.java
+++ AtCoder/other/typecal90/az092/Main.java
@@ -49,21 +49,5 @@ public class Main {
         long nextLong() {
             return Long.parseLong(next());
         }
-
-        double nextDouble() {
-            return Double.parseDouble(next());
-        }
-
-        String nextLine() {
-            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
-                try {
-                    return reader.readLine();
-                } catch (final IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-
-            return tokenizer.nextToken("\n");
-        }
     }
 }
