d90ed2ea0fabc61fb34844a05606c85c415df7ea
T45K <tasktas9@gmail.com>
2021-06-23 15:08:33 +0900

    update

---
+++ AtCoder/other/typecal90/bh060/Main.java
@@ -0,0 +1,168 @@
+package AtCoder.other.typecal90.bh060;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.function.BinaryOperator;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Main {
+  public static void main(final String[] args) {
+    final FastScanner scanner = new FastScanner(System.in);
+    final int n = scanner.nextInt();
+    final List<Integer> list = Stream.generate(scanner::nextInt)
+        .limit(n)
+        .collect(Collectors.toList());
+    final int[] natural = count(list);
+    Collections.reverse(list);
+    final int[] reverse = count(list);
+    final int answer = IntStream.range(0, n)
+        .map(i -> natural[i] + reverse[n - i - 1] - 1)
+        .max()
+        .orElseThrow();
+    System.out.println(answer);
+  }
+
+  private static int[] count(final List<Integer> list) {
+    final SegmentTree<Integer> tree = new SegmentTree<>(list.size() + 1, 0, Math::max);
+    return list.stream()
+        .map(value -> new Pair(value, tree.query(0, value) + 1))
+        .peek(pair -> tree.update(pair.value, pair.max))
+        .mapToInt(pair -> pair.max)
+        .toArray();
+  }
+
+  private static class Pair {
+    final int value;
+    final int max;
+
+    Pair(final int value, final int max) {
+      this.value = value;
+      this.max = max;
+    }
+  }
+
+  private static class SegmentTree<T> {
+    private final T[] internalArray;
+    private final int exponent;
+    private final T initialValue;
+    private final BinaryOperator<T> comparator;
+
+    SegmentTree(final List<T> list, final T initialValue, final BinaryOperator<T> comparator) {
+      this.exponent = 1 << Integer.toBinaryString(list.size() - 1).length();
+      this.comparator = comparator;
+      this.initialValue = initialValue;
+      internalArray = initArray(list, initialValue);
+    }
+
+    SegmentTree(final int size, final T initialValue, final BinaryOperator<T> comparator) {
+      this.exponent = 1 << Integer.toBinaryString(size - 1).length();
+      this.comparator = comparator;
+      this.initialValue = initialValue;
+      internalArray = initArray(Collections.emptyList(), initialValue);
+    }
+
+    /**
+     * 値の更新
+     *
+     * @param index "0-indexed"のインデックス
+     * @param value 更新後の値
+     */
+    void update(final int index, final T value) {
+      internalArray[index + exponent] = value;
+      int current = (index + exponent) / 2;
+      while (current > 0) {
+        internalArray[current] = comparator.apply(internalArray[current * 2], internalArray[current * 2 + 1]);
+        current /= 2;
+      }
+    }
+
+    /**
+     * 値の更新
+     *
+     * @param index    "0-indexed"のインデックス
+     * @param operator 更新式
+     */
+    void update(final int index, final UnaryOperator<T> operator) {
+      internalArray[index + exponent] = operator.apply(internalArray[index + exponent]);
+      int current = (index + exponent) / 2;
+      while (current > 0) {
+        internalArray[current] = comparator.apply(internalArray[current * 2], internalArray[current * 2 + 1]);
+        current /= 2;
+      }
+    }
+
+    /**
+     * クエリ
+     * クエリの区間を [left, right) の半開区間で渡すことに注意
+     *
+     * @param left  "0-indexed"のクエリの左端
+     * @param right "0-indexed"のクエリの右端 + 1
+     *              つまり"1-indexed"のクエリの右端
+     * @return クエリ結果
+     */
+    T query(final int left, final int right) {
+      return query(left, right, 0, exponent, 1);
+    }
+
+    T query(final int left, final int right, final int begin, final int end, final int k) {
+      if (left >= end || right <= begin) {
+        return initialValue;
+      }
+
+      if (left <= begin && end <= right) {
+        return internalArray[k];
+      }
+
+      final int mid = (begin + end) / 2;
+      return comparator.apply(query(left, right, begin, mid, k * 2), query(left, right, mid, end, k * 2 + 1));
+    }
+
+    @SuppressWarnings("unchecked")
+    private T[] initArray(final List<T> list, final T initialValue) {
+      final Object[] array = new Object[exponent * 2];
+      Arrays.fill(array, initialValue);
+      for (int i = 0; i < list.size(); i++) {
+        array[i + exponent] = list.get(i);
+      }
+
+      for (int i = exponent - 1; i > 0; i--) {
+        array[i] = comparator.apply((T) array[i * 2], (T) array[i * 2 + 1]);
+      }
+
+      return (T[]) array;
+    }
+  }
+
+  private static class FastScanner {
+    private final BufferedReader reader;
+    private StringTokenizer tokenizer;
+
+    FastScanner(final InputStream in) {
+      reader = new BufferedReader(new InputStreamReader(in));
+    }
+
+    String next() {
+      if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (final IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+  }
+}
