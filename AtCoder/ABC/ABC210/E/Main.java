019ef2972b3828640bdcc34037560d7a4c526e26
T45K <tasktas9@gmail.com>
2021-07-18 14:10:34 +0900

    update

--- AtCoder/ABC/ABC210/E/Main.java
+++ AtCoder/ABC/ABC210/E/Main.java
@@ -4,24 +4,66 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-// TODO
+/*
+解説AC
+最小全域木
+ユーザ解説がめちゃくちゃ分かりやすい
+https://blog.hamayanhamayan.com/entry/2021/07/17/233253
+ */
 public class Main {
     public static void main(final String[] args) {
         final FastScanner scanner = new FastScanner(System.in);
         final int n = scanner.nextInt();
         final int m = scanner.nextInt();
-        final long[] operations = new long[m + 1];
-        for (int i = 1; i <= m; i++) {
-            operations[i] = scanner.nextInt();
+        final List<Operation> operations = Stream.generate(() -> new Operation(scanner.nextInt(), scanner.nextInt()))
+            .limit(m)
+            .sorted(Comparator.comparingLong(o -> o.cost))
+            .collect(Collectors.toList());
+
+        final long answer = recursive(operations, 0, n, 0);
+        System.out.println(answer);
+    }
+
+    private static long recursive(final List<Operation> operations, final int index, final long connected, final long costSum) {
+        if (connected == 1) {
+            return costSum;
         }
 
-        final boolean containsEvenAndOdd = Arrays.stream(operations, 1, operations.length)
-            .map(i -> i % 2)
-            .distinct()
-            .count() == 2;
+        if (index == operations.size()) {
+            return -1;
+        }
+
+        final Operation operation = operations.get(index);
+        final long gcd = euclideanAlgorithm(connected, operation.length);
+        return recursive(operations, index + 1, gcd, costSum + (connected - gcd) * operation.cost);
+    }
+
+    private static long euclideanAlgorithm(final long a, final long b) {
+        if (b > a) {
+            return euclideanAlgorithm(b, a);
+        }
+
+        if (b == 0) {
+            return a;
+        }
+
+        return euclideanAlgorithm(b, a % b);
+    }
+
+    private static class Operation {
+        final int length;
+        final long cost;
+
+        Operation(final int length, final long cost) {
+            this.length = length;
+            this.cost = cost;
+        }
     }
 
     private static class FastScanner {
@@ -45,26 +87,6 @@ public class Main {
 
         int nextInt() {
             return Integer.parseInt(next());
-        }
-
-        long nextLong() {
-            return Long.parseLong(next());
-        }
-
-        double nextDouble() {
-            return Double.parseDouble(next());
-        }
-
-        String nextLine() {
-            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
-                try {
-                    return reader.readLine();
-                } catch (final IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-
-            return tokenizer.nextToken("\n");
         }
     }
 }
