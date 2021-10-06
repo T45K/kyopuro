ca91e48de84dc33868f4d4649436a629e83e717d
T45K <tasktas9@gmail.com>
2021-10-06 23:13:05 +0900

    update

--- AtCoder/ABC/ABC219/B/Main.java
+++ AtCoder/ABC/ABC219/B/Main.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(final String[] args) {
@@ -13,10 +15,12 @@ public class Main {
         final String s2 = scanner.next();
         final String s3 = scanner.next();
         final String[] s = {s1, s2, s3};
-        for (final char c : scanner.next().toCharArray()) {
-            System.out.print(s[c - '1']);
-        }
-        System.out.println();
+        final String t = scanner.next();
+        final String answer = IntStream.range(0, t.length())
+            .map(i -> t.charAt(i) - '1')
+            .mapToObj(i -> s[i])
+            .collect(Collectors.joining());
+        System.out.println(answer);
     }
 
     private static class FastScanner {
