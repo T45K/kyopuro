789cdd777fd805ce440f0de80d64c78667502ac4
T45K <tasktas9@gmail.com>
2021-10-06 19:32:47 +0900

    update

--- AtCoder/ARC/ARC120/C/Main.java
+++ AtCoder/ARC/ARC120/C/Main.java
@@ -6,58 +6,52 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Deque;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
+import java.util.function.BinaryOperator;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-// TODO solve
+/*
+Aの数字を一旦左側に寄せてから，Bの左側から順に当てはめていく
+この時の移動にかかるコストはBjの位置とAiの位置と，Aiより右側にあってすでに位置が決まってるものの総計から決まる
+ */
 public class Main {
     public static void main(final String[] args) {
         final FastScanner scanner = new FastScanner(System.in);
         final int n = scanner.nextInt();
-        final long[] a = Stream.generate(scanner::nextInt)
+        final List<Integer> a = Stream.generate(scanner::nextInt)
             .limit(n)
-            .mapToLong(Integer::intValue)
-            .toArray();
-        final long[] b = Stream.generate(scanner::nextInt)
+            .collect(Collectors.toList());
+        final List<Integer> b = Stream.generate(scanner::nextInt)
             .limit(n)
-            .mapToLong(Integer::intValue)
-            .toArray();
-        if (Arrays.stream(a).sum() != Arrays.stream(b).sum()) {
-            System.out.println(-1);
-            return;
+            .collect(Collectors.toList());
+
+        final Map<Integer, Deque<Integer>> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            final int value = a.get(i) + i;
+            map.computeIfAbsent(value, v -> new ArrayDeque<>()).addLast(i);
         }
 
-        if (Arrays.equals(a, b)) {
-            System.out.println(0);
-            return;
-        }
-
-        long swap = 0;
-        final Map<Long, Deque<Integer>> map = new HashMap<>();
-        int index = 0;
-        loop:
-        for (int i = 0; i < n; i++) { // b
-            final long bValue = b[i];
-            if (map.containsKey(bValue + i) && !map.get(bValue + i).isEmpty()) {
-                final Integer integer = map.get(bValue + i).pollFirst();
-                swap += integer - (i - 1);
-                continue;
+        final SegmentTree<Integer> tree = new SegmentTree<>(n, 0, Integer::sum);
+        long sum = 0;
+        for (int i = 0; i < b.size(); i++) {
+            final int bValue = b.get(i) + i;
+            final Deque<Integer> pairs = map.get(bValue);
+            if (pairs == null || pairs.isEmpty()) {
+                System.out.println(-1);
+                return;
             }
-            while (index < n) {
-                if (bValue == a[index] + index - i) {
-                    swap += index - i;
-                    continue loop;
-                }
-                map.computeIfAbsent(a[index] + index, k -> new ArrayDeque<>()).add(index);
-                index++;
-            }
-            System.out.println(-1);
-            return;
+            final int poll = pairs.pollFirst();
+            final int calibrated = poll + tree.query(poll + 1, n);
+            sum += Math.abs(calibrated - i);
+            tree.update(poll, 1);
         }
-        System.out.println(swap);
+        System.out.println(sum);
     }
 
     private static class FastScanner {
@@ -82,25 +76,75 @@ public class Main {
         int nextInt() {
             return Integer.parseInt(next());
         }
+    }
 
-        long nextLong() {
-            return Long.parseLong(next());
+    private static class SegmentTree<T> {
+        private final T[] internalArray;
+        private final int exponent;
+        private final T initialValue;
+        private final BinaryOperator<T> comparator;
+
+        SegmentTree(final int size, final T initialValue, final BinaryOperator<T> comparator) {
+            this.exponent = 1 << Integer.toBinaryString(size - 1).length();
+            this.comparator = comparator;
+            this.initialValue = initialValue;
+            internalArray = initArray(Collections.emptyList(), initialValue);
         }
 
-        double nextDouble() {
-            return Double.parseDouble(next());
+        /**
+         * 値の更新
+         *
+         * @param index "0-indexed"のインデックス
+         * @param value 更新後の値
+         */
+        void update(final int index, final T value) {
+            internalArray[index + exponent] = value;
+            int current = (index + exponent) / 2;
+            while (current > 0) {
+                internalArray[current] = comparator.apply(internalArray[current * 2], internalArray[current * 2 + 1]);
+                current /= 2;
+            }
         }
 
-        String nextLine() {
-            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
-                try {
-                    return reader.readLine();
-                } catch (final IOException e) {
-                    throw new RuntimeException(e);
-                }
+        /**
+         * クエリ
+         * クエリの区間を [left, right) の半開区間で渡すことに注意
+         *
+         * @param left  "0-indexed"のクエリの左端
+         * @param right "0-indexed"のクエリの右端 + 1
+         *              つまり"1-indexed"のクエリの右端
+         * @return クエリ結果
+         */
+        T query(final int left, final int right) {
+            return query(left, right, 0, exponent, 1);
+        }
+
+        T query(final int left, final int right, final int begin, final int end, final int k) {
+            if (left >= end || right <= begin) {
+                return initialValue;
             }
 
-            return tokenizer.nextToken("\n");
+            if (left <= begin && end <= right) {
+                return internalArray[k];
+            }
+
+            final int mid = (begin + end) / 2;
+            return comparator.apply(query(left, right, begin, mid, k * 2), query(left, right, mid, end, k * 2 + 1));
+        }
+
+        @SuppressWarnings("unchecked")
+        private T[] initArray(final List<T> list, final T initialValue) {
+            final Object[] array = new Object[exponent * 2];
+            Arrays.fill(array, initialValue);
+            for (int i = 0; i < list.size(); i++) {
+                array[i + exponent] = list.get(i);
+            }
+
+            for (int i = exponent - 1; i > 0; i--) {
+                array[i] = comparator.apply((T) array[i * 2], (T) array[i * 2 + 1]);
+            }
+
+            return (T[]) array;
         }
     }
 }
