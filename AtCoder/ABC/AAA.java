b98067e883fabd52c373135a84168aa6037a55e8
T45K <tasktas9@gmail.com>
2021-11-02 20:28:45 +0900

    update

---
+++ AtCoder/ABC/AAA.java
@@ -0,0 +1,15 @@
+package AtCoder.ABC;
+
+public class AAA {
+    public static void main(String[] args) {
+        final Hoge hoge = new Hoge() {
+
+        };
+    }
+
+    public static abstract class Hoge {
+        void aaa() {
+            System.out.println("bbb");
+        }
+    }
+}
