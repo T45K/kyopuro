17f3b41dab8548e3ebbd7474af105c3d3e3482e4
T45K <tasktas9@gmail.com>
2021-12-06 20:21:55 +0900

    update

---
+++ AtCoder/ABC/ABC229/E/Main.java
@@ -0,0 +1,127 @@
+package AtCoder.ABC.ABC229.E;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.StringTokenizer;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(final String[] args) {
+        final FastScanner scanner = new FastScanner(System.in);
+        final int n = scanner.nextInt();
+        final int m = scanner.nextInt();
+        final ListMap<Integer, Integer> graph = new ListMap<>();
+        for (int i = 0; i < m; i++) {
+            final int a = scanner.nextInt();
+            final int b = scanner.nextInt();
+            graph.putSingle(Math.min(a, b), Math.max(a, b));
+        }
+
+        final List<Integer> answers = new ArrayList<>();
+        answers.add(0);
+        int count = 0;
+        final UnionFindTree tree = new UnionFindTree(n);
+        for (int i = n; i >= 2; i--) {
+            count++;
+            for (final int node : graph.getList(i)) {
+                if (tree.getRoot(node) != i) {
+                    count--;
+                }
+                tree.unit(node, i);
+            }
+            answers.add(count);
+        }
+        Collections.reverse(answers);
+        final String answer = answers.stream()
+            .map(Objects::toString)
+            .collect(Collectors.joining("\n"));
+        System.out.println(answer);
+    }
+
+    private static class UnionFindTree {
+        private final int[] nodes;
+        private final Deque<Integer> indices = new ArrayDeque<>();
+
+        UnionFindTree(final int numOfNodes) {
+            this.nodes = IntStream.rangeClosed(0, numOfNodes).toArray();
+        }
+
+        /**
+         * 引数のノードが属している木の根を返す．
+         *
+         * @param nodeNumber ノードの番号
+         * @return 根，つまり属している集合の中の一番小さい値
+         */
+        int getRoot(final int nodeNumber) {
+            final int rootNode = nodes[nodeNumber];
+            if (rootNode != nodeNumber) {
+                indices.add(nodeNumber);
+                return getRoot(rootNode);
+            }
+
+            final Consumer<Integer> updateRoot = index -> nodes[index] = rootNode;
+            indices.forEach(updateRoot);
+            indices.clear();
+            return nodeNumber;
+        }
+
+        /**
+         * 引数のノードが属する集合を合体させる．
+         *
+         * @param nodeA ノード
+         * @param nodeB ノード
+         */
+        void unit(final int nodeA, final int nodeB) {
+            final int rootA = getRoot(nodeA);
+            final int rootB = getRoot(nodeB);
+            nodes[Math.max(rootA, rootB)] = Math.min(rootA, rootB);
+        }
+    }
+
+    private static class ListMap<K, V> extends HashMap<K, List<V>> {
+
+        public void putSingle(final K key, final V value) {
+            super.computeIfAbsent(key, k -> new ArrayList<>()).add(value);
+        }
+
+        public List<V> getList(final K key) {
+            return Optional.ofNullable(super.get(key)).orElse(Collections.emptyList());
+        }
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
