004c9a37cc84c5d81e4d90ed539b38ab3058c326
T45K <tasktas9@gmail.com>
2022-05-07 14:31:54 +0900

    update

--- AtCoder/ABC/ABC245/F/Main.java
+++ AtCoder/ABC/ABC245/F/Main.java
@@ -4,42 +4,59 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
+import java.util.Deque;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+import java.util.Map;
+import java.util.Set;
 import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-// TODO: solve
+/*
+「出ていく辺がない -> 行き止まり -> それ以上移動できない」になる
+また、行き止まりである頂点に向かう辺しかない頂点も同様に条件に合わない頂点になる
+なので、出ていく辺がない頂点を取り除く -> 出ていく辺の数を再計算する -> 出ていく辺がない頂点を取り除く -> ...
+を繰り返す
+
+トポロジカルソートに似た発想
+ */
 public class Main {
     public static void main(final String[] args) {
         final FastScanner scanner = new FastScanner(System.in);
         final int n = scanner.nextInt();
         final int m = scanner.nextInt();
-        final ListMap<Integer, Integer> graph = new ListMap<>();
+
+        final Map<Integer, Set<Integer>> parentToChildMap = new HashMap<>();
+        final Map<Integer, List<Integer>> childToParentMap = new HashMap<>();
         for (int i = 0; i < m; i++) {
             final int u = scanner.nextInt();
             final int v = scanner.nextInt();
-            graph.putSingle(u, v);
+            parentToChildMap.computeIfAbsent(u, k -> new HashSet<>()).add(v);
+            childToParentMap.computeIfAbsent(v, k -> new ArrayList<>()).add(u);
         }
 
-        final int[] array = new int[n + 1];
-        Arrays.fill(array, -1);
-        array[1] = 0;
-    }
+        final Deque<Integer> queue = IntStream.rangeClosed(1, n)
+            .filter(i -> !parentToChildMap.containsKey(i))
+            .boxed()
+            .collect(Collectors.toCollection(ArrayDeque::new));
 
-    private static class ListMap<K, V> extends HashMap<K, List<V>> {
-
-        public void putSingle(final K key, final V value) {
-            super.computeIfAbsent(key, k -> new ArrayList<>()).add(value);
+        while (!queue.isEmpty()) {
+            final int childToBeDeleted = queue.pollFirst();
+            for (final int parent : childToParentMap.getOrDefault(childToBeDeleted, Collections.emptyList())) {
+                parentToChildMap.get(parent).remove(childToBeDeleted);
+                if (parentToChildMap.get(parent).isEmpty()) {
+                    parentToChildMap.remove(parent);
+                    queue.add(parent);
+                }
+            }
         }
 
-        public List<V> getList(final K key) {
-            return Optional.ofNullable(super.get(key)).orElse(Collections.emptyList());
-        }
+        System.out.println(parentToChildMap.size());
     }
 
     private static class FastScanner {
@@ -63,53 +80,6 @@ public class Main {
 
         int nextInt() {
             return Integer.parseInt(next());
-        }
-
-        long nextLong() {
-            return Long.parseLong(next());
-        }
-
-        double nextDouble() {
-            return Double.parseDouble(next());
-        }
-
-        String nextLine() {
-            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
-                try {
-                    return reader.readLine();
-                } catch (final IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-
-            return tokenizer.nextToken("\n");
-        }
-    }
-
-    private static class Pair<T1, T2> {
-        final T1 first;
-        final T2 second;
-
-        Pair(final T1 first, final T2 second) {
-            this.first = first;
-            this.second = second;
-        }
-
-        @Override
-        public boolean equals(final Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            final Pair<?, ?> pair = (Pair<?, ?>) o;
-            return Objects.equals(first, pair.first) && Objects.equals(second, pair.second);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(first, second);
         }
     }
 }
