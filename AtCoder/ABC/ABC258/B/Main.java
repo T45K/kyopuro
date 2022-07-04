28495227f7d5e13039a013b6f85addfddc8f3f79
T45K <tasktas9@gmail.com>
2022-07-04 21:19:20 +0900

    update

---
+++ AtCoder/ABC/ABC258/B/Main.java
@@ -0,0 +1,61 @@
+package AtCoder.ABC.ABC258.B;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.stream.IntStream;
+
+public class Main {
+    private static final int[] X_MOVE = {0, 0, 1, 1, 1, -1, -1, -1};
+    private static final int[] Y_MOVE = {1, -1, 0, 1, -1, 0, 1, -1};
+
+    public static void main(final String[] args) {
+        final FastScanner scanner = new FastScanner(System.in);
+        final int n = scanner.nextInt();
+        final int[][] table = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            final char[] chars = scanner.next().toCharArray();
+            for (int j = 0; j < n; j++) {
+                table[i][j] = chars[j] - '0';
+            }
+        }
+
+        final long answer = IntStream.range(0, n).boxed()
+            .flatMap(i -> IntStream.range(0, n).boxed()
+                .flatMap(j -> IntStream.range(0, 8)
+                    .mapToLong(k -> IntStream.range(0, n)
+                        .mapToLong(l -> table[(n + i + X_MOVE[k] * l) % n][(n + j + Y_MOVE[k] * l) % n] * (long) Math.pow(10, n - l - 1))
+                        .sum()
+                    ).boxed()
+                )
+            ).max(Long::compare)
+            .orElseThrow();
+        System.out.println(answer);
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
---
+++ AtCoder/ABC/ABC258/D/Main.java
@@ -0,0 +1,86 @@
+package AtCoder.ABC.ABC258.D;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Objects;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(final String[] args) {
+        final FastScanner scanner = new FastScanner(System.in);
+        final int n = scanner.nextInt();
+        final int x = scanner.nextInt();
+        final List<Pair<Long, Long>> list = Stream.generate(() -> new Pair<>(scanner.nextLong(), scanner.nextLong()))
+            .limit(n)
+            .collect(Collectors.toList());
+
+        long min = Long.MAX_VALUE;
+        long acc = 0;
+        for (int i = 0; i < Math.min(n, x); i++) {
+            final Pair<Long, Long> pair = list.get(i);
+            acc += pair.first + pair.second;
+            min = Math.min(min, acc + (x - i - 1) * pair.second);
+        }
+        System.out.println(min);
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+
+    private static class Pair<T1, T2> {
+        final T1 first;
+        final T2 second;
+
+        Pair(final T1 first, final T2 second) {
+            this.first = first;
+            this.second = second;
+        }
+
+        @Override
+        public boolean equals(final Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            final Pair<?, ?> pair = (Pair<?, ?>) o;
+            return Objects.equals(first, pair.first) && Objects.equals(second, pair.second);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(first, second);
+        }
+    }
+}
