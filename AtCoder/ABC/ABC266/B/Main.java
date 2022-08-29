d6eb7eb91202d2e09437a02286b108dfe6fc857a
T45K <tasktas9@gmail.com>
2022-08-29 20:39:30 +0900

    update

---
+++ AtCoder/ABC/ABC266/B/Main.java
@@ -0,0 +1,45 @@
+package AtCoder.ABC.ABC266.B;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static final int MOD = 998_244_353;
+
+    public static void main(final String[] args) {
+        final FastScanner scanner = new FastScanner(System.in);
+        final long n = scanner.nextLong();
+        final long mod = n > 0
+            ? n % MOD
+            : (n - MOD * (n / MOD - 1)) % MOD;
+        System.out.println(mod);
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
