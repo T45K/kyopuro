0f30384e5d73be5f1c01aff07d92836adbba1c8f
T45K <tasktas9@gmail.com>
2023-03-25 22:11:52 +0900

    update

---
+++ AtCoder/ABC/ABC295/A/Main.java
@@ -0,0 +1,60 @@
+package AtCoder.ABC.ABC295.A;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(final String[] args) {
+        final FastScanner scanner = new FastScanner(System.in);
+        final int n = scanner.nextInt();
+        final boolean answer = Stream.generate(scanner::next)
+            .limit(n)
+            .anyMatch(Main::match);
+        if (answer) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+
+    private static boolean match(final String word) {
+        switch (word) {
+            case "and":
+            case "not":
+            case "that":
+            case "the":
+            case "you":
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
