cd9daf2e548d5f6225b4fab99792fd250ee4d314
T45K <tasktas9@gmail.com>
2023-04-09 23:05:43 +0900

    update

---
+++ AtCoder/ABC/ABC297/A/Main.java
@@ -0,0 +1,54 @@
+package AtCoder.ABC.ABC297.A;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(final String[] args) {
+        final FastScanner scanner = new FastScanner(System.in);
+        final int n = scanner.nextInt();
+        final int d = scanner.nextInt();
+        final List<Integer> list = Stream.generate(scanner::nextInt)
+            .limit(n)
+            .collect(Collectors.toList());
+
+        final Integer answer = IntStream.range(1, n)
+            .filter(i -> list.get(i) - list.get(i - 1) <= d)
+            .boxed()
+            .findFirst()
+            .map(list::get)
+            .orElse(-1);
+        System.out.println(answer);
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
---
+++ AtCoder/ABC/ABC297/E/Main.java
@@ -0,0 +1,68 @@
+package AtCoder.ABC.ABC297.E;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(final String[] args) {
+        final FastScanner scanner = new FastScanner(System.in);
+        final int n = scanner.nextInt();
+        final int k = scanner.nextInt();
+        final List<Long> list = Stream.generate(scanner::nextLong)
+            .limit(n)
+            .distinct()
+            .collect(Collectors.toList());
+
+        final Set<Long> exists = new HashSet<>(list);
+        final PriorityQueue<Long> queue = new PriorityQueue<>(list);
+        for (int i = 0; i < k - 1; i++) {
+            assert !queue.isEmpty();
+            final long poll = queue.poll();
+            for (final long value : list) {
+                if (!exists.contains(poll + value)) {
+                    queue.add(poll + value);
+                    exists.add(poll + value);
+                }
+            }
+        }
+
+        System.out.println(queue.poll());
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
